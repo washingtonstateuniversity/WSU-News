39e01bc322fd871275b1bb399be63e0cfd168c38
jeremyfelt <jeremy.felt@gmail.com>
2013-12-12 14:34:09 -0800

    Parse through the content of each post to look for individual images
    Displays the image source for each along with the ID

--- web/wp-content/mu-plugins/wsu-news-content-fix.php
+++ web/wp-content/mu-plugins/wsu-news-content-fix.php
@@ -40,16 +40,26 @@ class Content_Fix extends WP_CLI_Command {
 			$limit = 10;
 		}
 
-		$like_query = "LIKE '%$src_url%'";
+		$like_query = "LIKE '%src=\"$src_url%'";
 		$query   = "SELECT ID, post_content FROM {$wpdb->posts} WHERE post_content {$like_query} LIMIT $limit";
-		$results = $wpdb->get_results( $query );
-
+		echo $query;
+		echo "\n";
+		$results = $wpdb->get_results( $query );var_dump( $results );
+		$inc = 0;
 		foreach( $results as $result ) {
-			echo $result->post_content;
-			echo "\n";
+			$xml_parser = xml_parser_create();
+			xml_parse_into_struct( $xml_parser, $result->post_content, $pieces );
+			foreach( $pieces as $piece ) {
+				if ( 'IMG' === $piece['tag'] ) {
+					echo $result->ID . ': ' . $piece['attributes']['SRC'];
+					echo "\n";
+					$inc++;
+				}
+			}
+
 		}
 
-		WP_CLI::success( $src_url );
+		WP_CLI::success( $inc . ' images' );
 	}
 }
 
