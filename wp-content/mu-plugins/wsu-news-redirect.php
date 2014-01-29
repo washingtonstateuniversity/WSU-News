e0ff3a95651c98473292e279e16ec8191389b941
jeremyfelt <jeremy.felt@gmail.com>
2014-01-29 14:35:46 -0800

    Use 301 redirects rather than 302 redirects now that we're settled

--- wp-content/mu-plugins/wsu-news-redirect.php
+++ wp-content/mu-plugins/wsu-news-redirect.php
@@ -22,7 +22,7 @@ function wsu_redirect_publication_id() {
 		$publication_id = absint( $_GET['PublicationID'] );
 		$post_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM wp_postmeta WHERE meta_key = '_publication_id' AND meta_value = %s", $publication_id ) );
 		if ( 0 !== absint( $post_id ) ) {
-			wp_safe_redirect( get_permalink( $post_id ) );
+			wp_safe_redirect( get_permalink( $post_id ), 301 );
 			exit;
 		}
 	}
@@ -36,7 +36,7 @@ function wsu_redirect_publication_id() {
 		$publication_id = absint( $urlparts[4] );
 		$post_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM wp_postmeta WHERE meta_key = '_publication_id' AND meta_value = %s", $publication_id ) );
 		if ( 0 !== absint( $post_id ) ) {
-			wp_safe_redirect( get_permalink( $post_id ) );
+			wp_safe_redirect( get_permalink( $post_id ), 301 );
 			exit;
 		}
 	}    
