6e0ab1da01d83e398372b617f0801a28997f0695
jeremyfelt <jeremy.felt@gmail.com>
2014-03-25 14:01:51 -0700

    Update script version

--- wp-content/themes/wsu-news-twentythirteen/functions.php
+++ wp-content/themes/wsu-news-twentythirteen/functions.php
@@ -9,7 +9,7 @@ class WSU_News_Twentythirteen {
 	/**
 	 * @var string Version of CSS / JS for cache breaking.
 	 */
-	var $script_version = '20140321-04';
+	var $script_version = '20140321-06';
 
 	/**
 	 * Hook in where needed when the theme is loaded.
