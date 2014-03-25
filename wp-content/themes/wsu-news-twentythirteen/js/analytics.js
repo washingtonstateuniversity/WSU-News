69258e9c80ab1b3be9e858b105871c52f0bd7242
jeremyfelt <jeremy.felt@gmail.com>
2014-03-25 14:04:13 -0700

    Bring over param() function from bootstrap

--- wp-content/themes/wsu-news-twentythirteen/js/analytics.js
+++ wp-content/themes/wsu-news-twentythirteen/js/analytics.js
@@ -1,4 +1,7 @@
-(function($){
+(function($, window){
+
+	function param( name , process_url ){if(typeof(process_url)==='undefined'){process_url=window.location.href;}name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");var regexS = "[\\?&]"+name+"=([^&#]*)";var regex = new RegExp( regexS );var results = regex.exec( process_url );if( results == null ){ return false;}else{return results[1];}}
+
 	var url = 'https://news.wsu.edu/bootstrap/bootstrap_v3.js?gacode=UA-6322839-2&amp;loading=element_v2&amp;domainName=news.wsu.edu/';
 	var GAcode = param("gacode", url );
 	var _load  = param("loading", url );
@@ -12,4 +15,4 @@
 		$.jtrack.defaults.debug.console = true;
 		$.jtrack({ load_analytics:{account:GAcode},options:jQuery.extend({},(_DN!=false?{'domainName':_DN}:{}),(_CP!=false?{'cookiePath':_CP}:{})), trackevents:data });
 	});
-})(jQuery);
+})(jQuery, window);
